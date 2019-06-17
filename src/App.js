66a4faac4faba400ce99f5558fe81db2b88aaa38
johnjsirjohn <ionut.sirbu@hotmail.com>
2019-06-17 12:34:06 +0200

    Adding the albumslist

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux';
 import store from './store';
+import AlbumsListContainer from './components/AlbumsListContainer';
 
 class App extends React.Component {
   render() {
+    if (this.props.albums === null) {
+      return 'Loading...';
+    }
     return (
       <Provider store={store}>
         <div className='App'>
           <header className='App-header'>
-            <img src={logo} className='App-logo' alt='logo' />
-            <h1 className='App-title'>Welcome to React</h1>
+            <h1 className='App-title'>Hello</h1>
           </header>
-          <p className='App-intro'>
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+          <AlbumsListContainer />
         </div>
       </Provider>
     );
