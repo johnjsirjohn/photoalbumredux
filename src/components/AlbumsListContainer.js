970ffff10836e44612ff5435a85773c876c90861
johnjsirjohn <ionut.sirbu@hotmail.com>
2019-06-17 14:25:30 +0200

    Action creators - Redux actions

--- src/components/AlbumsListContainer.js
+++ src/components/AlbumsListContainer.js
@@ -1,14 +1,18 @@
 import * as React from 'react';
 import * as request from 'superagent';
 import AlbumsList from './AlbumsList';
+import { connect } from 'react-redux';
+import { helloWorld } from '../actions/test';
 
-export default class AlbumsListContainer extends React.Component {
+class AlbumsListContainer extends React.Component {
   state = {};
 
   componentDidMount() {
+    this.props.helloWorld('Alice', 'Seriously Alice');
     request('https://jsonplaceholder.typicode.com/albums').then(response =>
       this.setState({ albums: response.body })
     );
+    //console.log(helloWorld('ll', 'df'));
   }
 
   render() {
@@ -16,3 +20,8 @@ export default class AlbumsListContainer extends React.Component {
     return <AlbumsList albums={this.state.albums} />;
   }
 }
+
+export default connect(
+  null,
+  { helloWorld }
+)(AlbumsListContainer);
